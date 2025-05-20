ae51ef704acf9033de2cad0c4739a0eee8ca17b1
Mohammad-Fares <163373532+Mohammad-Fares@users.noreply.github.com>
2025-05-20 15:16:44 +1000

    created functioning post method, yet hashing error (v0.7)
    the post method has been created and functions as intended, yet an error has been experienced when attempting to hash password and store it.

--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,16 @@ const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
 const cors = require('cors');
 const path = require('path');
-const regsubmit = require(`./frontend/script.js`);
 const bcrypt = require('bcrypt');
 
-const saltRounds = 11;
-
 const app = express();
-const port = 3001;
+
+const port = 3000;
 
 app.use(express.json());
 app.use(cors());
+
+app.use(express.static(path.join(__dirname, '..', 'frontend')));
 
 const dbPath = path.join(__dirname, 'database', 'users.db'); /*Creating a table for users*/
 const db = new sqlite3.Database(dbPath, (err) => {
@@ -29,21 +29,15 @@ const db = new sqlite3.Database(dbPath, (err) => {
     }
 });
 
-bcrypt.genSalt(saltRounds, (err, salt) => { /*Generating the salt*/
-    if (err) {
-        return;
-    };
-});
+
 
 
 app.post('/api/register', (req, res) => { /* register post function */
     const { name, email, password, role } = req.body;
-    const hashPass = password;
-    bcrypt.hash(hashPass, salt, (err, hash) => {
-        if (err) {
-            return;
-        }
-    })
+    
+    const saltRounds = 10;
+    const salt = bcrypt.genSalt(saltRounds);
+    const hashPass  = bcrypt.hash(password, salt)
     const sql = `INSERT INTO users (name, email, password, role) VALUES (?,?,?,?)`
     db.run(sql, 
         [name, email, hashPass, role],
@@ -65,3 +59,7 @@ app.post('/api/register', (req, res) => { /* register post function */
 if (regsubmit = 1) {
     pass
 }*/
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
